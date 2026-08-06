c0c3cce6a801f6b80b0ab8b259de6a53408601ab
m4shaikh <moin.shaikh.3656@gmail.com>
2026-08-07 04:07:16 +0530

    l |

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import '../global.css'
+
+export default function Index() {
+  return (
+    <View className="flex flex-1 items-center justify-center">
+      <Text>Edit 578455werer app/index.tsx jjjto edit this screen.</Text>
+    </View>
+  );
+}
