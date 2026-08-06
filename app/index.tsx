bb55b8595bc571624131da8a36e95747210d9316
m4shaikh <moin.shaikh.3656@gmail.com>
2026-08-07 04:20:25 +0530

    fixed nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
 import '../global.css'
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex flex-1 items-center justify-center">
+    <View className="flex flex-1 items-center justify-center bg-red">
       <Text>Edit 578455werer app/index.tsx jjjto edit this screen.</Text>
     </View>
   );
